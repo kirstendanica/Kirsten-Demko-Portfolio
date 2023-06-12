9901e02f33125e3ca8df39b2140f818441808602
kirsten d <kirdanic@gmail.com>
2023-06-12 19:56:20 -0400

    Contact form update

--- script.js
+++ script.js
@@ -11,3 +11,38 @@ const showMenu = (toggleId, navId) => {
     };
     
     showMenu('.toggler', '.toggler-open');
+
+
+/* -- Contact form -- */
+document.addEventListener("DOMContentLoaded", function () {
+  const ajaxForms = document.querySelectorAll("form[data-ajax]");
+
+  ajaxForms.forEach((form) => {
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      const formData = new FormData(form);
+      const action = form.getAttribute("data-netlify") ? "/?no-cache=1" : form.action;
+
+      fetch(action, {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => {
+          if (response.ok) {
+            // Form submitted successfully
+            alert("Thank you! : )");
+            form.reset();
+          } else {
+            // Error submitting form
+            alert("Hmm, looks like there was a mistake... Please try again!");
+          }
+        })
+        .catch((error) => {
+          // Network error
+          console.error("Error submitting your information", error);
+          alert("There was a network error submittin your information. Please try again!");
+        });
+    });
+  });
+});
