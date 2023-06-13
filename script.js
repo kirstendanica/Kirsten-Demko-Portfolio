6ad4b7108b20dfb9f145249e13c2aa8d175c26ac
kirsten d <kirdanic@gmail.com>
2023-06-12 20:13:25 -0400

    Update 'submit' javascript

--- script.js
+++ script.js
@@ -17,13 +17,23 @@ const showMenu = (toggleId, navId) => {
 document.addEventListener("DOMContentLoaded", function () {
   const ajaxForms = document.querySelectorAll("form[data-ajax]");
 
+  function showAlert(message, timeout = 3000) {
+    const alertElement = document.getElementById("custom-alert");
+    alertElement.textContent = message;
+    alertElement.classList.remove("hidden");
+
+    setTimeout(() => {
+      alertElement.classList.add("hidden");
+    }, timeout);
+  }
+
   ajaxForms.forEach((form) => {
     form.addEventListener("submit", (event) => {
       event.preventDefault();
 
       const formData = new FormData(form);
       formData.append("no-cache", "1"); // Add the no-cache field
-      const action = form.getAttribute("data-netlify") ? "/" : form.action;
+      const action = form.getAttribute("data-netlify") ? "/submit" : form.action;
 
       fetch(action, {
         method: "POST",
@@ -32,17 +42,17 @@ document.addEventListener("DOMContentLoaded", function () {
         .then((response) => {
           if (response.ok) {
             // Form submitted successfully
-            alert("Form submitted successfully!");
+            showAlert("Form submitted successfully!");
             form.reset();
           } else {
             // Error submitting form
-            alert("There was an error submitting the form. Please try again.");
+            showAlert("There was an error submitting the form. Please try again.");
           }
         })
         .catch((error) => {
           // Network error
           console.error("Error submitting form:", error);
-          alert("There was a network error submitting the form. Please try again.");
+          showAlert("There was a network error submitting the form. Please try again.");
         });
     });
   });
