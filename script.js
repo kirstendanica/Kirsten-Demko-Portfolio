430d7e280772efc34d8b7aabe2ddd914c642f965
kirsten d <kirdanic@gmail.com>
2023-06-13 20:50:32 -0400

    Update

--- script.js
+++ script.js
@@ -60,3 +60,43 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+/* -- Projects -- */
+const imageWraps = document.querySelectorAll('.image-wrap');
+
+const scrollAmount = 100; // Amount to scroll in pixels
+
+imageWraps.forEach((imageWrap) => {
+  const scrollCont = imageWrap.querySelector('.scroll-cont');
+  const arrowUp = imageWrap.querySelector('.arrow-up');
+  const arrowDown = imageWrap.querySelector('.arrow-down');
+  const linkWrap = imageWrap.querySelector('.link-wrap');
+  const link = linkWrap.querySelector('a');
+
+  const centerClickAreaSize = 50; // Size in percentage, adjust as needed
+
+  scrollCont.addEventListener('click', (event) => {
+    const { offsetX, offsetY } = event;
+    const rect = scrollCont.getBoundingClientRect();
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const deltaX = Math.abs(centerX - offsetX);
+    const deltaY = Math.abs(centerY - offsetY);
+
+    if (
+      deltaX <= (rect.width * centerClickAreaSize) / 100 / 2 &&
+      deltaY <= (rect.height * centerClickAreaSize) / 100 / 2
+    ) {
+      link.click();
+    }
+  });
+
+  arrowUp.addEventListener('click', () => {
+    scrollCont.scrollTop -= scrollAmount;
+  });
+
+  arrowDown.addEventListener('click', () => {
+    scrollCont.scrollTop += scrollAmount;
+  });
+});
